688708a3ab54d71b9ae524ca044a8f92004167d2
JoeBulley <bulleyyy@gmail.com>
2022-02-08 12:13:43 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorials) => {
+  return tutorials.charAt(0).toUppercase() + tutorials.slice(1);
 }
+
+const capital = tutorials.split(' ').map(captialize).join(' ');
+
+capital;
+
